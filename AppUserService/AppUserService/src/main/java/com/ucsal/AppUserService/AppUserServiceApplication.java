514673febb9d4c275921490523aff58a93c4edfd
GustavoCruzDev <gustavocruz.silva@ucsal.edu.br>
2025-05-28 15:27:01 -0300

    first commit

---
+++ AppUserService/AppUserService/src/main/java/com/ucsal/AppUserService/AppUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ucsal.AppUserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppUserServiceApplication.class, args);
+	}
+
+}
