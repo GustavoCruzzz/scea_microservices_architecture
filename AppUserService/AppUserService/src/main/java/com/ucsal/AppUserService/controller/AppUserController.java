8ec57b00be77fa108d37b14a36f35ff6b37a3c9c
Caspiom <lucasamsg6@gmail.com>
2025-06-01 15:52:29 -0300

    commit

--- AppUserService/AppUserService/src/main/java/com/ucsal/AppUserService/controller/AppUserController.java
+++ AppUserService/AppUserService/src/main/java/com/ucsal/AppUserService/controller/AppUserController.java
@@ -1,4 +1,20 @@
 package com.ucsal.AppUserService.controller;
 
+
+import com.maviniciusdev.back.registration.token.ConfirmationTokenService;
+import com.ucsal.AppUserService.service.AppUserService;
+import jakarta.annotation.security.PermitAll;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@AllArgsConstructor
 public class AppUserController {
 }
