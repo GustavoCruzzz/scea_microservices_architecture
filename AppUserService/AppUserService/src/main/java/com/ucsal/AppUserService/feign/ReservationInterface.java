b23e207e4d842da04dbd2e37c0f0fe73b55b9b57
Caspiom <lucasamsg6@gmail.com>
2025-06-01 15:51:54 -0300

    commit

--- AppUserService/AppUserService/src/main/java/com/ucsal/AppUserService/feign/ReservationInterface.java
+++ AppUserService/AppUserService/src/main/java/com/ucsal/AppUserService/feign/ReservationInterface.java
@@ -1,4 +1,13 @@
 package com.ucsal.AppUserService.feign;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Map;
+
+@FeignClient(name = "RESERVATIONSERVICE")
 public interface ReservationInterface {
 }
